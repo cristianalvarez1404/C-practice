afa10654b5ad5545c0fec6587e3805bca33f13fd
cristian <cristianalvarez1496@gmail.com>
2025-10-28 15:53:51 -0500

    enums

--- 10enums.c
+++ 10enums.c
@@ -13,13 +13,19 @@ enum escapes {
   NEWLINE = '\n',
   VTAB = '\v',
   RETURN = '\r'
-}
+};
+
+
 
 int main(){
   long int num = 0;
   const int num2 = 123456679L;
   int num3 = 0x1f;
   char text[] = "hola mundo";
+
+  const char msg[] = "warning : ";
+
+  
 
   printf("%d\n",num3);
   printf("%d\n", strlen(text));
