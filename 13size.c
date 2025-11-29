cbd2929e488267b9b8749392c6d6f0ccffd1bdce
cristian <cristianalvarez1496@gmail.com>
2025-11-29 14:56:57 -0500

    size files

---
+++ 13size.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*
+int main(){
+  int var = 0X43FF;
+  printf("%X\n",var);
+  printf("%d",var);
+  
+  return 0;
+}
+*/
+
+/*
+static int i;
+static int i = 27;
+static int i;
+
+int main(){
+  static int i;
+  printf("%d", i);
+  printf("%s","Hello world!");
+  return 0;
+}
+*/
+
+static int i = 24;
+int main() {
+
+  return 0;
+}
