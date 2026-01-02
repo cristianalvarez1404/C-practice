710cc1f243218dc40ae7532e7c0ef0e93e5554d9
CristianAlvarez <cristianandres1496@gmail.com>
2026-01-02 17:52:15 -0500

    data structures types

---
+++ 108.data_structes-types.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+  //A data structure is the organization of the data in a way so that it can be used efficently. => IN TERMS OF TIME REPEAT TIME AND SPACE
+
+  //STACK ADT IS IMPLEMENTED USIGN ARRAYS OR LINKED LISTS
+
+  //REMEMBER THIS => ADT (client) tell us WHAT is to be done and DATA STRUCTURE(back) tells us HOW to do it
+
+  //The question is this => How to know which data structure to use for a particular ADT?
+
+  // TYPE OF DATA STRUCTURES
+  /*
+    Linear Data structures:elements are arranged in a linear sequential order each element has a ONE PREDECESSOR AND ONE SUCCESSOR EXCEPT THE ONE AND THE LAST ONE. => Examples => arrays, queues, linked list, stack.
+
+    Non Linear Data structures:elements are not arranged in a linear(sequential) order. => Example => Tree(nodes dont have predecessor and successor, it has TWO successors), Graph
+  
+    Static data structures: The memory is allocated at COMPILE TIME. Therefore, maximum size is fixed. => FAST ACCESS, SLOW INSERTION AND DELETION. Example => We have an array and we HAVE TO FIX THE SIZE BEFORHAND {1,2,3,4,5}, IN OTHER WORDS, WE ARE CREATING THE CODE IN COMPILE TIME
+
+    Dynamic data structures: The memory is allocated at RUN TIME. Therefore, maximum size is flexible. FASTER AND INSERTION AND DELETION, BUT SLOWER ACCESS => Example => Linked list
+
+
+
+  */
+
+
+  return 0;
+}
