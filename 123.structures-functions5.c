b9d0a4932769dd5069be6d89ace06156ebb7c87d
cristian <cristianalvarez1496@gmail.com>
2026-01-08 17:54:16 -0500

    structures and functions5

---
+++ 123.structures-functions5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct point {
+  int x;
+  int y;
+};
+
+struct point* fun(int a, int b){
+  struct point *ptr = (struct point *)malloc(sizeof(struct point));
+  ptr->x = a;
+  ptr->y = b + 5;
+  
+  return ptr;
+}
+
+void print(struct point *ptr){
+  printf("%d %d\n", ptr->x, ptr->y);
+}
+
+int main(){
+  //Returning a pointer to a structure from the function
+  //Remember that stack memory automatically gets deallocated but HEAP MEMORY will not, it will remain intact.
+
+
+  struct point *ptr1, *ptr2;
+  ptr1 = fun(2, 3);
+  ptr2 = fun(6, 9);
+  print(ptr1); print(ptr2);
+  free(ptr1); 
+  free(ptr2);
+
+  return 0;
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+  // Passing array of structures as argument.
+
+  return 0;
+}
