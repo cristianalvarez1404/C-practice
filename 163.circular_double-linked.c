e49bba932e280d8d1f0df5a4599a5397ee913765
cristian <cristianalvarez1496@gmail.com>
2026-01-23 16:49:39 -0500

    circular linked list

---
+++ 163.circular_double-linked.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  struct node* prev;
+  int data;
+  struct node* next;
+};
+
+struct node* circularDoubly(int data){
+  struct node* temp = malloc(sizeof(struct node));
+  temp->data = data;
+  temp->next = temp;
+  temp->prev = temp;
+  return temp;
+}
+
+int main(){
+  int data = 45;
+  struct node* tail;
+  tail = circularDoubly(data);
+
+  printf("%d\n", tail->data);
+  return 0;
+}
