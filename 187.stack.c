51717fd7d9f60e98d03b2fca9e9694761a5c0f8c
cristian <cristianalvarez1496@gmail.com>
2026-02-06 16:20:08 -0500

    stack

---
+++ 187.stack.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  //Stack => A stack is a linear data structure in which insertions and deletions are allowed only at the end, called the top of the stack.
+
+  //Stack as an ADT => When we define a stack as an ADT (or Abstract Data Type), then we are only interested in knowing the stack operations from the user point of view.
+
+  /*
+    Primary stack operations
+      -push(data) => insert data onto stack.
+      -pop() => Deletes the last inserted element from the stack.
+      -top() => Returns the last inserted element without removing it.
+      -size() => Returns the size or the number of elements in the stack.
+      -isEmpty() => Returns TRUE if the stack is empty, else returns FALSE.
+      -isFull() => Returns TRUE if the stack is full, else returns FALSE. 
+  */
+
+  return 0;
+}
