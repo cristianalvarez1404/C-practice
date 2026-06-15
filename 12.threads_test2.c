0fd75d6cd856d377baadb34fcd1b34f726910a4c
cristiann <cristianandres1496@gmail.com>
2026-06-15 16:17:13 -0500

    threads with server http

---
+++ 12.threads_test2.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
+#include <stdint.h>
+#include <pthread.h>
+
+//standard HTTP port
+#define SERVER_PORT 80
+
+#define MAXLINE 4096
+#define SA struct sockaddr
+
+#define BIG 1000000UL
+uint32_t counter = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+void *count_to_big(void *arg) {
+  for(uint32_t i = 0; i < BIG; i++){
+    counter++;
+  }
+  return NULL;
+}
+
+char *getHomePage(char *ipaddr, int *bytesread){
+  int sockfd, n;
+  int sendbytes;
+  struct sockaddr_in servaddr;
+  char sendline[MAXLINE];
+  char *buffer = malloc(MAXLINE);
+
+  if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    return NULL
+  }
+
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(SERVER_PORT);
+
+  if(inet_pton(AF_INET, ipaddr, &servaddr.sin_addr) <= 0){
+    return NULL;
+  }
+
+  if(connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) < 0){
+    return NULL;
+  }
+
+  //We're connected. Prepare the message.
+  sprintf(sendline, "GET / HTTP/1.1\r\n\r\n");
+  sendbytes = strlen(sendline);
+
+  //Send the request
+  if(write(sockfd, sendline, sendbytes) != sendbytes){
+    return NULL;
+  }
+
+  memset(buffer, 0, MAXLINE);
+
+  //Now read the first MAXLINE server's response.
+  n = read(sockfd, buffer, MAXLINE - 1);
+  if(n < 0)
+    return NULL;
+  
+  *bytesread = n;
+
+  return buffer;
+}
+
+int main() {
+  pthread_t t;
+  char *buf;
+  int buflen;
+
+  pthread_create(&t, NULL, count_to_big, NULL);
+  //count_to_big(NULL);
+  buf = getHomePage("000.000.0.00", &buflen);
+  pthread_join(t, NULL);
+  printf("Done. Counter = %u. Recved %d bytes\n", counter, buflen);
+
+}
