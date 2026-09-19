4f8e96da77e0b2fe75a55bd724e54336f338260a
cristiann <cristianandres1496@gmail.com>
2026-09-19 16:19:40 -0500

    shell C

--- programs_C/main.c
+++ programs_C/main.c
@@ -3,45 +3,98 @@
 #include <unistd.h>
 #include <string.h>
 
+int handle_redirect(char *args[]);
+
 int main(int argc, char *argv[])
 {
-  //String to hold input buffer
-  char buf[1024];
+  int interactive = 1;
 
-  // Prompt
-  printf("$ ");
-
-  // Get input from keyboard
-  fgets(buf, 1024, stdin);
-
-  // Trim newline
-  char *nl = strchr(buf, '\n');
-  if(nl) *nl = '\0';
-
-  // Split buf into args
-  char *args[20];
-  int nargs = 0;
-
-  args[nargs] = strtok(buf, " ");
-  while(args[nargs] != NULL)
+  if(argc > 1)
   {
-    args[++nargs] = strtok(NULL, " ");
+    // First arg is the name of the script
+    if(freopen(argv[1], "r", stdin) == NULL)
+    {
+      fprintf(stderr, "Can't read from script file %s\n", argv[1]);
+      fprintf(stderr, "Exiting.\n");
+      exit(1);
+    }
+    interactive = 0;
   }
 
-  // Fork and exec
-  pid_t pid = fork();
+  while(1)
+  {
+    //String to hold input buffer
+    char buf[1024];
 
-  if(pid > 0)
-  {
-    // We are the parent
-    wait(NULL);
-  } 
-  else
-  {
-    // We are the child
-    execlp(buf, buf, NULL);
-    
-    // Something went wrong with exec
-    fprintf(stderr, "Could not exec %s\n", buf);
+    // Prompt
+    if(interactive) printf("$ ");
+
+    // Get input from keyboard
+    if(fgets(buf, 1024, stdin) == NULL) break;
+
+    // Trim newline
+    char *nl = strchr(buf, '\n');
+    if(nl) *nl = '\0';
+
+    // Remove comments
+    char *hash = strchr(buf, '#');
+    if(hash) *hash = '\0';
+
+    // Split buf into args
+    char *args[20];
+    int nargs = 0;
+
+    args[nargs] = strtok(buf, " ");
+    while(args[nargs] != NULL)
+    {
+      args[++nargs] = strtok(NULL, " ");
+    }
+
+    if(args[0] == NULL) continue;
+    if(strcmp(args[0], "exit") == 0) exit(0);
+
+    // Fork and exec
+    pid_t pid = fork();
+
+    if(pid > 0)
+    {
+      // We are the parent
+      wait(NULL);
+    } 
+    else
+    {
+      // We are the child
+      if(handle_redirect(args) == -1){
+        fprintf(stderr, "Could not redirect\n");
+        exit(1);
+      }
+
+      execlp(args[0], args);
+      
+      // Something went wrong with exec
+      fprintf(stderr, "Could not exec %s\n", buf);
+    }
   }
+}
+
+// Handle redirect stdout
+// Return 1 if successful
+// Return 0 is no redirect was needed
+// Return -1 if something broke
+int handle_redirect(char *args[]) {
+  for(int i = 0; args[i] != NULL; i++){
+    if(strcmp(args[i], ">") == 0)
+    {
+      // Assume next arg is the file to write to
+      if(freopen(args[i + 1], "w", stdout) == NULL) return -1;
+
+      if(stdout == NULL) return -1;
+
+      // Terminate args at the ">" symbol
+      args[i] = NULL;
+
+      return 1;
+    }
+  }
+  return 0;
 }
